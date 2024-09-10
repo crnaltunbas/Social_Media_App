c7111b744b8ae069c056adbbe0867b2ee3620032
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-09-10 22:30:15 +0300

    Uygulamada kullanabileceğimiz renk paleti hex kodu ile colors.dart dosyasında eklendi.

---
+++ android/app/src/main/kotlin/com/example/social_media_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.social_media_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/assets/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SocialMediaAppColors {
+  static const Color primaryColor =
+  Color(0xFFFFCAD4);
+  static const Color secondColor =
+  Color(0xFFF4ACB7);
+  static const Color thirdColor =
+  Color(0xFF9D8189);
+  static const Color fourthColor =
+  Color(0xFFFFE5D9);
+  static const Color fifthColor =
+  Color(0xFFD8E2DC);
+}
