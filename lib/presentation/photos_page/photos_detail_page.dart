d3ec7485cdc2b0dd1d1853f7df1add8da0a8dca4
nehirkilic <kilicnehr@gmailcom>
2024-09-21 03:10:47 +0300

    photos_controller icindeki eksiklikler giderildi. photos_page tasarımı yapıldı ve photos_detail_page oluşturulup tasarlandı. repository guncellendi.

--- lib/presentation/photos_page/photos_detail_page.dart
+++ lib/presentation/photos_page/photos_detail_page.dart
@@ -8,8 +8,8 @@ class PhotosDetailPage extends StatelessWidget {
   const PhotosDetailPage({Key? key, required this.photo}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-  final theme = Theme.of(context);
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       backgroundColor: SocialMediaAppColors.white,
       appBar: AppBar(
@@ -29,10 +29,12 @@ class PhotosDetailPage extends StatelessWidget {
                 photo.url ?? '',
                 fit: BoxFit.contain,
                 errorBuilder: (context, error, stackTrace) {
-                  return const Center(
+                  return Center(
                     child: Text(
                       'Image not available',
-                      style: TextStyle(color: SocialMediaAppColors.grey),
+                      style: theme.textTheme.bodyMedium?.copyWith(
+                        color: SocialMediaAppColors.grey,
+                      ),
                     ),
                   );
                 },
@@ -40,7 +42,7 @@ class PhotosDetailPage extends StatelessWidget {
             ),
           ),
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16 ),
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
             decoration: const BoxDecoration(
               color: SocialMediaAppColors.fifthColor,
               borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
@@ -50,25 +52,25 @@ class PhotosDetailPage extends StatelessWidget {
               children: [
                 Text(
                   photo.title ?? 'No Title',
-                  style: const TextStyle(
+                  style: theme.textTheme.titleMedium?.copyWith(
                     color: SocialMediaAppColors.thirdColor,
                     fontWeight: FontWeight.bold,
-                    fontSize: 16,
                   ),
                 ),
                 const SizedBox(height: 5),
                 Text(
                   'Album Id: ${photo.albumId}',
-                  style: const TextStyle(color: SocialMediaAppColors.thirdColor, fontSize: 13.7),
+                  style: theme.textTheme.bodyMedium?.copyWith(
+                    color: SocialMediaAppColors.thirdColor,
+                  ),
                 ),
                 const SizedBox(height: 20),
                 Divider(color: SocialMediaAppColors.thirdColor),
                 const SizedBox(height: 3),
-                const Text(
+                Text(
                   'Comments Section',
-                  style: TextStyle(
+                  style: theme.textTheme.bodySmall?.copyWith(
                     color: SocialMediaAppColors.thirdColor,
-                    fontSize: 13.6,
                     fontStyle: FontStyle.italic,
                   ),
                 ),
@@ -80,3 +82,4 @@ class PhotosDetailPage extends StatelessWidget {
     );
   }
 }
+
